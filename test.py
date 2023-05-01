57e6e60f8a8b9199f81c818da5655058698dc272
woutvos <mail@woutvos.nl>
2023-05-01 21:18:52 +0200

    Remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import hashlib
-
-def check_leerling(leerling, code):
-    print(
-        'SELECT * FROM leerlingen WHERE leerlingnummer = %s AND code = %s;' % (leerling, code)
-    )
-
-check_leerling(1, 2)
